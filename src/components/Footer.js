8805ef5160ae85c6d955e6f037b621649e4b4041
Paula Davidson <99604021+Calla-P@users.noreply.github.com>
2023-03-05 12:13:33 -0800

    Revert "Nickwork"

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Footer.css';
-
-class Footer extends React.Component{
-  render(){
-    return <footer> &copy; KitchenIt @ Code  Fellows ~ All Rights Reserved &middot; 2023</footer>;
-  }
-}
-export default Footer;
